df22e28cf00caac5931927bbe970f33bb0c4806f
KMDSOUF <kamad.soufiane@gmail.com>
2016-11-11 20:02:39 +0100

    Interface graphique

--- CRUD_BLOB_AZURE/Form1.Designer.cs
+++ CRUD_BLOB_AZURE/Form1.Designer.cs
@@ -28,20 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Conteneur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TailleBlob = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_vider = new System.Windows.Forms.Button();
+            this.btn_supprimer = new System.Windows.Forms.Button();
+            this.btn_envoyer = new System.Windows.Forms.Button();
+            this.btn_actualiser = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_parcourir = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Location = new System.Drawing.Point(34, 207);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(967, 248);
+            this.panel3.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Conteneur,
+            this.Nom,
+            this.TailleBlob,
+            this.URL});
+            this.dataGridView1.Location = new System.Drawing.Point(18, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(946, 235);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Conteneur
+            // 
+            this.Conteneur.HeaderText = "Conteneur";
+            this.Conteneur.Name = "Conteneur";
+            // 
+            // Nom
+            // 
+            this.Nom.HeaderText = "Nom";
+            this.Nom.Name = "Nom";
+            // 
+            // TailleBlob
+            // 
+            this.TailleBlob.HeaderText = "Taille blob";
+            this.TailleBlob.Name = "TailleBlob";
+            // 
+            // URL
+            // 
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.Width = 400;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_vider);
+            this.panel1.Controls.Add(this.btn_supprimer);
+            this.panel1.Controls.Add(this.btn_envoyer);
+            this.panel1.Controls.Add(this.btn_actualiser);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btn_parcourir);
+            this.panel1.Location = new System.Drawing.Point(114, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(814, 142);
+            this.panel1.TabIndex = 14;
+            // 
+            // btn_vider
+            // 
+            this.btn_vider.Location = new System.Drawing.Point(492, 102);
+            this.btn_vider.Name = "btn_vider";
+            this.btn_vider.Size = new System.Drawing.Size(152, 29);
+            this.btn_vider.TabIndex = 11;
+            this.btn_vider.Text = "Vider";
+            this.btn_vider.UseVisualStyleBackColor = true;
+            // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Location = new System.Drawing.Point(334, 102);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Size = new System.Drawing.Size(152, 29);
+            this.btn_supprimer.TabIndex = 10;
+            this.btn_supprimer.Text = "Supprimer";
+            this.btn_supprimer.UseVisualStyleBackColor = true;
+            // 
+            // btn_envoyer
+            // 
+            this.btn_envoyer.Location = new System.Drawing.Point(176, 102);
+            this.btn_envoyer.Name = "btn_envoyer";
+            this.btn_envoyer.Size = new System.Drawing.Size(152, 29);
+            this.btn_envoyer.TabIndex = 3;
+            this.btn_envoyer.Text = "Envoyer";
+            this.btn_envoyer.UseVisualStyleBackColor = true;
+            // 
+            // btn_actualiser
+            // 
+            this.btn_actualiser.Location = new System.Drawing.Point(650, 102);
+            this.btn_actualiser.Name = "btn_actualiser";
+            this.btn_actualiser.Size = new System.Drawing.Size(152, 29);
+            this.btn_actualiser.TabIndex = 10;
+            this.btn_actualiser.Text = "Charger la table";
+            this.btn_actualiser.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Racine du fichier";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(205, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(597, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Conteneur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nom du fichier";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Yellow;
+            this.textBox2.Location = new System.Drawing.Point(205, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(597, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btn_parcourir
+            // 
+            this.btn_parcourir.Location = new System.Drawing.Point(18, 102);
+            this.btn_parcourir.Name = "btn_parcourir";
+            this.btn_parcourir.Size = new System.Drawing.Size(152, 29);
+            this.btn_parcourir.TabIndex = 0;
+            this.btn_parcourir.Text = "Parcourir";
+            this.btn_parcourir.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1035, 514);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Conteneur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TailleBlob;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_vider;
+        private System.Windows.Forms.Button btn_supprimer;
+        private System.Windows.Forms.Button btn_envoyer;
+        private System.Windows.Forms.Button btn_actualiser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_parcourir;
     }
 }
 
