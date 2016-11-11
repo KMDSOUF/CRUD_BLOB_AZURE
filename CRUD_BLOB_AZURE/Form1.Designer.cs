78d9994d369020ada8dad2f8d5745ca891b72d91
KMDSOUF <kamad.soufiane@gmail.com>
2016-11-11 19:40:13 +0100

    ajout de packages : Bibliothèque cliente Azure Storage pour .NET; Gestionnaire de configuration Azure pour .NET;

--- CRUD_BLOB_AZURE/Form1.Designer.cs
+++ CRUD_BLOB_AZURE/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 481);
+            this.ClientSize = new System.Drawing.Size(1035, 514);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
